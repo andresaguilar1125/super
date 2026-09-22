96bea882ba0ac4c073fba3121145846f878fa0d3
andresaguilar1125 <67489487+andresaguilar1125@users.noreply.github.com>
2026-09-22 13:47:27 -0600

    Updates

---
+++ _app/immutable/entry/start.CQplMT0G.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/B0mxDVaE.js";import{x as t}from"../chunks/DeaHZagK.js";export{t as load_css,r as start};
---
+++ _app/immutable/nodes/2.C0Yes9ZH.js
@@ -0,0 +1,1 @@
+import{s as e,o as s}from"../chunks/OPkkAcBP.js";import{S as a,i as r}from"../chunks/3cpyxNta.js";import{g as n}from"../chunks/B0mxDVaE.js";import{b as i}from"../chunks/DeaHZagK.js";function m(o){return s(()=>{n(`${i}/calculator`,{replaceState:!0})}),[]}class f extends a{constructor(t){super(),r(this,t,m,null,e,{})}}export{f as component};
