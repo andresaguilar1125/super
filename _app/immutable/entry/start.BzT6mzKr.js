41da7fc78fe4191182eec59bdfd85e56c828e900
andresaguilar1125 <67489487+andresaguilar1125@users.noreply.github.com>
2026-09-22 13:45:07 -0600

    Updates

--- _app/immutable/entry/start.BzT6mzKr.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/B8LuZHR4.js";import{x as t}from"../chunks/B4O7Yj67.js";export{t as load_css,r as start};
---
+++ _app/immutable/entry/start.CoNmDVV3.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/C4p2THgZ.js";import{x as t}from"../chunks/CJmiO8tU.js";export{t as load_css,r as start};
